c6513fae683d3fe24924afb812f11fed820d47fd
Christopher Chang <chiggiechang@gmail.com>
2021-10-01 22:07:33 -0700

    Format Main.hs with ormolu

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,27 @@
 module Main where
 
-import Lib
+import AbsDiceExpr
+import Control.Monad
+import Data.List
+import ErrM
 import LexDiceExpr
+import Lib
 import ParDiceExpr
 import PrintDiceExpr
-import AbsDiceExpr
-import ErrM
-
-import System.Random
 import System.Exit
-
-import Data.List
-
-import Control.Monad
+import System.Random
 
 type Verbosity = Int
+
 type ParseFun a = [Token] -> Err a
 
 putStrV :: Verbosity -> String -> IO ()
 putStrV v s = when (v > 1) $ putStrLn s
 
 showTree :: (Show a, Print a) => Int -> a -> IO ()
-showTree v tree
- = do
-      putStrV v $ "\n[Abstract Syntax]\n\n" ++ show tree
-      -- putStrV v $ "\n[Linearized tree]\n\n" ++ printTree tree
+showTree v tree =
+  do
+    putStrV v $ "\n[Abstract Syntax]\n\n" ++ show tree
 
 execCBN :: Program -> Integer
 execCBN (Prog p) = interpret p
@@ -39,26 +36,29 @@ interpret (EModifier n) = n
 interpret (EPlus exp1 exp2) = interpret exp1 + interpret exp2
 
 run :: Verbosity -> ParseFun Program -> String -> IO ()
-run v p s = let ts = myLexer s in case p ts of
-           Bad s    -> do putStrLn "\nParse Failed...\n"
-                          putStrV v "Tokens:"
-                          putStrV v $ show ts
-                          putStrLn s
-                          exitFailure
-           Ok  tree -> do 
-                          putStrLn "\nInput:"
-                          putStrLn $ printTree tree
-                          -- putStrLn "\nParse Successful!"
-                          showTree v tree
-                          putStrLn "\nTokens:"
-                          putStrV v $ show ts
-                          putStrLn "\nOutput:"
-                          putStrLn $ printTree $ execCBN tree
-                          -- putStrLn "\n Result computed using call by value:"
-                          -- putStrLn $ printTree $ execCBV tree
-                          exitSuccess
-           Left _ -> exitFailure
-           Right _ -> exitFailure
+run v p s =
+  let ts = myLexer s
+   in case p ts of
+        Bad s -> do
+          putStrLn "\nParse Failed...\n"
+          putStrV v "Tokens:"
+          putStrV v $ show ts
+          putStrLn s
+          exitFailure
+        Ok tree -> do
+          putStrLn "\nInput:"
+          putStrLn $ printTree tree
+
+          showTree v tree
+          putStrLn "\nTokens:"
+          putStrV v $ show ts
+
+          putStrLn "\nOutput:"
+          putStrLn $ printTree $ execCBN tree
+
+          exitSuccess
+        Left _ -> exitFailure
+        Right _ -> exitFailure
 
 -- https://www.schoolofhaskell.com/school/starting-with-haskell/libraries-and-frameworks/randoms
 -- main :: IO ()
